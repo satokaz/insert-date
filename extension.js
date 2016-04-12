eafbe63d089bb36fbe7dc59951e09e5e8af6ef4e
satokaz <kazus.sunfish@gmail.com>
2016-04-13 06:29:23 +0900

    update

--- extension.js
+++ extension.js
@@ -13,18 +13,33 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    var disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
+    // var disposable = vscode.commands.registerCommand('extension.sayHello', function () {
+    //     // The code you place here will be executed every time your command is executed
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+    //     // Display a message box to the user
+    //     vscode.window.showInformationMessage('Hello World!');
+    // });
 
-    context.subscriptions.push(disposable);
+    vscode.commands.registerCommand('extension.insertdate', function () {
+        vscode.window.activeTextEditor.edit(function (editBuilder) {
+            
+            //日付を取得して、Date object から文字列にへ変換し dateString に格納
+            //0x20 でスペースを追加
+            var dt = new Date();
+            var dateString = dt.toLocaleString() + "\x20";
+
+            //現在のカーソル位置(Line, character) を取得
+            var selection = vscode.window.activeTextEditor.selection;
+            var startLine = selection.start.line;
+            var startCharacter = selection.start.character;
+
+            //取得した Line と character を引数に vscode.Potion(); を実行して .intert に渡して done!
+            editBuilder.insert(new vscode.Position(startLine, startCharacter), dateString);
+        });
+		// Display a message box to the user
+		// vscode.window.showInformationMessage('Edit applied!');
+	});
+
+    // context.subscriptions.push(disposable);
 }
 exports.activate = activate;
-
-// this method is called when your extension is deactivated
-function deactivate() {
-}
-exports.deactivate = deactivate;
